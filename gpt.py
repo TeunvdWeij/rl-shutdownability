b4149d7ae069d68e65383bf12200f7118d26432e
TeunvdWeij <mailvanteun@gmail.com>
2023-03-21 09:52:24 +0100

    create gpt module

---
+++ gpt.py
@@ -0,0 +1,23 @@
+import os
+import openai
+
+
+class ChatApp:
+    def __init__(self, system_text, api_path="API_KEY.txt", model="gpt-3.5-turbo-0301"):
+        self.messages = [{"role": "sytem", "content": system_text}]
+
+        with open(api_path, "r") as file:
+            openai.api_key = file.readline().strip()
+
+        self.model = model
+
+    def chat(self, message):
+        self.messages.append({"role": "user", "content": message})
+        response = openai.ChatCompletion.create(
+            model=self.model, messages=self.messages, max_tokens=2, temperature=0
+        )
+
+        self.messages.append(
+            {"role": "assistant", "content": response["choices"][0]["message"]["content"]}
+        )
+        return response["choices"][0]["message"]["content"].strip()
